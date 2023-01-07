70834320a56bb6d810e434cdd19841d9d1dc821f
bermet <bermetabdyvasieva@gmail.com>
2023-01-07 11:09:38 +0600

    updates on tests

--- test/pageobjects/loginPage.js
+++ test/pageobjects/loginPage.js
@@ -1,12 +1,17 @@
 class LoginPage {
-get LoginField (){return $('input[name="userName"]')}
-get passwordField () {return $('input[name="password')}
-get submitButton () {return $ ('button[type="submit')}
+  get loginField () { return $('input[name="userName"]'); }
+  get passwordField () { return $('input[name="password"]'); }
+  get submitButton () { return $('button[type="submit"]'); }
 
-async dologin(userName="Admin", password="Admin@Navi"){
-  await this.LoginField.setValue(userName);
-  await this.passwordField.setValue(password);
-  await this.submitButton.click();
+  async doLogin(username="Admin", password="Admin@Navi"){
+      await this.loginField.setValue(username);
+      await this.passwordField.setValue(password);
+      await this.submitButton.click();
+  }
+
+  async open(){
+      await browser.url(browser.options.baseUrl + 'login');
+  }
 }
-}
+
 module.exports = new LoginPage();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -1,26 +1,21 @@
-const loginPage = require('../pageobjects/loginPage');
-const LoginPage = require('../pageobjects/loginPage')
+const LoginPage = require('../pageobjects/LoginPage');
 
+describe('Login page on Navigator CRM', () => {
+    beforeEach('Setup browser', async() =>{
+        await LoginPage.open();
+    })
+    afterEach('Clear browser', async()=>{
+        await browser.reloadSession();
+    })
 
-describe('Login page on Navigator CRM', () =>{
-  beforeEach('Setup browser', async()=>{
-    await browser.url('http://167.114.201.175:5000/login');
-  })
-  afterEach('Clear browser', async()=>{
-    await browser.reloadSession();
-  })
-  
-  it('should login', async()=>{
-    await LoginPage.dologin();
-    await expect(browser).toHaveUrlContaining('clients');
-  })
+    it('should login', async () => {
+        await LoginPage.doLogin();
+        await expect(browser).toHaveUrlContaining('clients');
+    })
 
-  it('should fail login', async()=>{
-    await loginPage.dologin("d", "p");
-    await expect(browser).toHaveAttrContaining('login');
-  })
+    it('should fail login', async () => {
+        await LoginPage.doLogin("Admin", "InvalidPassword");
+        await expect(browser).toHaveUrlContaining('login');
+    })
 })
-
- 
- 
  
