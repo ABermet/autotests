c2147f09cd42a521faebc6be1f76afd13cdf9cc3
bermet <bermetabdyvasieva@gmail.com>
2023-01-17 20:00:02 +0600

    Create clients

--- test/pageobjects/clientPage.js
+++ test/pageobjects/clientPage.js
@@ -5,9 +5,68 @@ class ClientPage {
   get surnameField() { return $('input[formcontrolname="userSurname"]')}
   get firstnameField() { return $('input[formcontrolname="userName"]')}
 
+  get genderRadio() { return $('mat-radio-group mat-radio-button div.mat-radio-label-content')}
+
+  get emailField() { return $('input[type="email"]')}
+  get phoneField() { return $('input[formcontrolname="phone"]')}
+  get birthField() { return $('input[formcontrolname="birthday"]')}
+  get middleNameField() { return $('input[formcontrolname="userMiddleName"]')}
+  get saveButton() { return $('button[name="save"]')}
+
+
+  get openChildModalPageButton() { return $('div.add-user-children span i')}
+  get childsModalPage() { return $('mat-dialog-container app-children-dialog')}
+  get childsModalPageAddChildOption() { return this.childsModalPage.$('button.mat-raised-button')}
+  get childEnterRow() { return this.childsModalPage.$('div.crm-row')}
+  get childNameField() { return this.childEnterRow.$('input[formcontrolname="Name"]')}
+  get childBirthDateField() { return this.childEnterRow.$('input[formcontrolname="Birthday"]')}
+  get childGenderSelect() { return this.childEnterRow.$('mat-select')}
+  get childGenderSelectOption() { return $('//mat-option/span[contains(text(),"М")]')}
+  get addChild() { return this.childsModalPage.$('//mat-dialog-actions/button[contains(text(),"Сохранить")]')}
+
+  get openSearch() {return $('form.search-form')}
+
+  //для редактирования
+get openFirstClientDetails() {return $('table.crm-navigator-table tbody tr')};
+get editModalPage() {return $('div[class="modal-window user"]')};
+
+//физическая карта
+// get editCardButton() {return $('div[class="client-data"] span[class="editIcon"]')};
+// get editCardField() {return $('div[class="client-data"] span[class="editIcon"]')}
+// get saveButton() {return $('button[name="mat-raised-button"]')}
+
+
+get editSurnameField() {return this.editModalPage.$('input[placeholder="Фамилия"]')};
+get editFirstnameField() {return this.editModalPage.$('input[placeholder="Имя"]')};
+
+get editPhoneButton() {return $('div[class="col-item tel"] span[class="editIcon"]')};
+get editPhoneField() {return $('[formcontrolname="phone"]')}
+get editemailButton() {return $('input [class="mat-input-element mat-form-field-autofill-control ng-pristine ng-valid ng-touched"]')};
+get editemailField() {return $('[formcontrolname="email"]')};
+get saveButton() {return $('//mat-dialog-actions/button[contains(text(),"Сохранить")]')}
+
+
+get editChildButton() {return $('div[class="block"] span[class="editIcon"]')};
+get childNameButton() {return $('mat-card-actions span[class=mat-button-wrapper]')}; 
+get saveButton() {return $('//mat-dialog-actions/button[contains(text(),"Сохранить")]')};
+
+// get editchildNameField() {return $('app-children-dialog input[class="mat-input-element mat-form-field-autofill-control ng-pristine ng-valid ng-touched"]')};
+
+
   // второй способ
   async fillForm(testingUser){
       await this.surnameField.setValue(testingUser.surname)
+  }
+
+  async fillChild(childName="Phillip", birthdate="05/06/2000"){
+      await this.openChildModalPageButton.click();
+      await this.childsModalPageAddChildOption.click();
+      await this.childNameField.setValue(childName);
+      await this.childBirthDateField.setValue(birthdate);
+      await this.childGenderSelect.click();
+      await this.childGenderSelectOption.waitForDisplayed({timeout:5000})
+      await this.childGenderSelectOption.click();
+      await this.addChild.click();
   }
 
   async open(){
--- test/specs/clients.spec.js
+++ test/specs/clients.spec.js
@@ -1,31 +1,86 @@
 const LoginPage = require('../pageobjects/LoginPage')
-const ClientPage = require('../pageobjects/ClientPage')
+const ClientPage = require('../pageobjects/ClientPage');
+const clientPage = require('../pageobjects/ClientPage');
 
 describe('Clients test', () => {
     beforeEach( async() => {
         await LoginPage.open();
         await LoginPage.doLogin();
-        await ClientPage.open();
+        // await ClientPage.open();
+    })
+    afterEach('Clear browser', async()=>{
+        await browser.reloadSession();
     })
 
-    it('should create new client', async ()=> {
+    xit('should create new client', async ()=> {
         // уже тут шаги открытия формы клиента и заполнения 
        // первый способ
+        // await browser.pause(5000);
+        // implicit , explicit
+        await ClientPage.createClientButton.waitForDisplayed({ timeout: 10000 });
         await ClientPage.createClientButton.click();
         await ClientPage.surnameField.setValue("Тестовая фамилия")
         await ClientPage.firstnameField.setValue("Тестовое имя")
+        await ClientPage.genderRadio.click();
+        await ClientPage.emailField.setValue("bellatrix.black.tester06@gmail.com");
+        await ClientPage.middleNameField.setValue("Отчество");
+        await ClientPage.birthField.setValue("05/06/1990");
+        await ClientPage.phoneField.setValue("996700555666");
+        await ClientPage.fillChild();
+        await ClientPage.saveButton.click(); 
+        await browser.pause(10000);
+        await browser.acceptAlert();
+
+
 
         //  второй способ
-        let testingUser = {
-            surname: "Тестовая фамилия"
-        }
-        await ClientPage.fillForm(testingUser)
+        // let testingUser = {
+        //     surname: "Тестовая фамилия"
+        // }
+        // await ClientPage.fillForm(testingUser)
         // окончание второго способ
-
-        await browser.pause(5000);
     })
 
-    it('remain test cases', async () => {
+    //редактирование клиента
+    it('should create new client', async () => {
+      await ClientPage.createClientButton.waitForDisplayed({timeout: 10000 });
+      await ClientPage.openFirstClientDetails.click();
+      //await ClientPage.editModalPage.waitForDisplayed({timeout: 10000});
+
+      //fiz karta
+      // await ClientPage.editCardButton.click(); 
+      // await browser.pause(10000);
+      // await ClientPage.editCardField.setValue("445566");
+      // await browser.pause(10000);
+
+
+      await ClientPage.editSurnameField.clearValue();
+      await ClientPage.editSurnameField.setValue("NEW SURNAME");
+      await ClientPage.editFirstnameField.clearValue();
+      await ClientPage.editFirstnameField.setValue("NEW NAME");
+
+      await ClientPage.editPhoneButton.click();
+      await ClientPage.editPhoneField.clearValue();
+      await ClientPage.editPhoneField.setValue("996700222333");
+      await ClientPage.editemailField.clearValue();
+      await ClientPage.editemailField.setValue("testing@mail.ru");  
+      await ClientPage.saveButton.click(); 
+
+      await ClientPage.editChildButton.click();
+      // await ClientPage.editchildNameField.clearValue();
+      // await ClientPage.editchildNameField.setValue("Lina");
+
+      await ClientPage.childNameButton.click();
+      await ClientPage.childNameField.setValue("Lina");
+      await ClientPage.childBirthDateField.setValue("01/01/2009")
+      await ClientPage.childGenderSelect.click();
+      await ClientPage.childGenderSelectOption.click();
+      await ClientPage.saveButton.click();
+      //await browser.pause(10000);
+
+
+      await browser.pause(10000);
+       
 
     })
 
